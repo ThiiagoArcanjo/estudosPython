3a8d58f69a08d57f12d561fa45edf5634cbcbd5a
Thiago Arcanjo De Oliveira <thiagokai.arcanjo@gmail.com>
2022-01-20 21:08:04 -0300

    Transformando em função

--- 1-equacaoDeSegundoGrau.py
+++ 1-equacaoDeSegundoGrau.py
@@ -6,29 +6,33 @@ Se o delta calculado for negativo a equação não possui valores reais. Informe
 Se o delta calculado for igual a zero a equação possui apenas uma raiz. Informe-a ao usuário
 Se o delta for positivo, a equação possi duas raizes; Informe-as ao usuário.
 '''
-print('\t\t *-* Programa Calculo de equação de segundo Grau *-*')
-a = float(input('Por favor, informe o valor para a: '))
 
-if(a == 0):
-    print('Que chato! Infelizmente a equação não é de segundo grau')
+def equacaoSegundoGrau(aParametro,bParametro,cParametro):
+    print('\t\t *-* Programa Calculo de equação de segundo Grau *-*')
+    a = aParametro
 
-else:
-    b = float(input('Por favor, informe o valor para b: '))
-    c= float(input('Por favor, informe o valor para c: '))
-    delta = pow(b,2)-4* a*c
-    print(f'Delta é: {delta}')
-   
-    if(delta < 0):
-        print('O delta possui um valor negativo, portanto não possui valores reais')
+    if(a == 0):
+        print('Que chato! Infelizmente a equação não é de segundo grau')
 
-    elif(delta == 0):
-        x = (-b + (delta** (1/2))) / (2*a)
-        #or x = -b /(2*a)
-        print('O delta possui valor igual a zero, portanto possui apenas uma raiz')
-        print(f'\t\t O X é igual: {x} ')
-        
     else:
-        x1 = (-b + (delta** (1/2))) / (2*a)
-        x2 = (-b - (delta** (1/2))) / (2*a)
-        print('O delta possui um valor positivo maior que zero, portanto possui duas raizes')
-        print(f'\t\t X1 é igual: {x1} \n \t\t e X2 é igual a: {x2}')   
+        b = bParametro
+        c =  cParametro
+        delta = pow(b,2)-4* a*c
+        print(f'Delta é: {delta}')
+    
+        if(delta < 0):
+            print('O delta possui um valor negativo, portanto não possui valores reais')
+
+        elif(delta == 0):
+            x = (-b + (delta** (1/2))) / (2*a)
+            #or x = -b /(2*a)
+            print('O delta possui valor igual a zero, portanto possui apenas uma raiz')
+            print(f'\t\t O X é igual: {x} ')
+            
+        else:
+            x1 = (-b + (delta** (1/2))) / (2*a)
+            x2 = (-b - (delta** (1/2))) / (2*a)
+            print('O delta possui um valor positivo maior que zero, portanto possui duas raizes')
+            print(f'\t\t X1 é igual: {x1} \n \t\t e X2 é igual a: {x2}')  
+
+equacaoSegundoGrau(float(input("Por favor, digite a: ")),float(input("Por favor, digite b: ")), float(input("Por favor, digite c: "))) 
